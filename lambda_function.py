2032849539c840f0a43d7f5bf38915f2ba25c631
Ubuntu <kobuika.icloud.com>
2020-01-20 05:39:17 +0000

    一旦完成

--- lambda_function.py
+++ lambda_function.py
@@ -1,19 +1,33 @@
 import json
 from tocaro_handler import TocaroHandler
 from cloudwatchevents_accessor import CloudWatchEventsAccessor
-
+import urllib.request
 
 def lambda_handler(event, context):
+    url = '' + event["id"] + '/'
+    
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as res:
+            body = json.load(res)
+    except urllib.error.HTTPError as err:
+        print(err.code)
+    except urllib.error.URLError as err:
+        print(err.reason)
     
     # tocaro投稿処理
     tocaro = TocaroHandler()
     
-    tocaro.set_text("test ID=" + str(event["id"]))
+    tocaro.set_text("※※※リマインドです※※※")
     tocaro.set_color("success")
     tocaro.set_attachments(
         [{
-            "title:": "test title",
-            "value": "test value"
+            "title": "日時",
+            "value": body["date"]
+        },
+        {
+            "title": "内容",
+            "value": body["text"]
         }
         ]
     )
@@ -26,6 +40,7 @@ def lambda_handler(event, context):
     b.delete_event(event["name"])
     
     return r
+
 
 
 if __name__ == '__main__':
