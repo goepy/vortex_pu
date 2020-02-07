3b18c813447535852885b75986b9803badb2a85f
sirayogi <toriboku@gmail.com>
2020-02-07 07:59:01 +0000

    イベント更新機能削除、削除機能修正1

--- reminder/reminder/api/views.py
+++ reminder/reminder/api/views.py
@@ -21,23 +21,25 @@ class TableViewSet(viewsets.ModelViewSet):
                 id_=serializer.data["uuid"])
         print("debug: perform_create")
     
-    def perform_update(self, serializer):
-        serializer.save()
-        b = CloudWatchEventsAccessor()
-        try:
-            b.delete_event("fy19pu-reminder-" + (datetime.strptime(serializer.data["date"], "%Y-%m-%dT%H:%M:%S+09:00") - timedelta(hours=9)).strftime("%Y%m%d%H%M"))
-        except:
-            print("debug: delete events is noting.")
-        
-        b.set_event(
-                date_=datetime.strptime(serializer.data["date"], "%Y-%m-%dT%H:%M:%S+09:00") - timedelta(hours=9),
-                id_=serializer.data["uuid"])
-        print("debug: perform_update")
+#    def perform_update(self, serializer):
+#        serializer.save()
+#        b = CloudWatchEventsAccessor()
+#        try:
+#            b.delete_event("fy19pu-reminder-" + (datetime.strptime(serializer.data["date"], "%Y-%m-%dT%H:%M:%S+09:00") - timedelta(hours=9)).strftime("%Y%m%d%H%M"))
+#        except:
+#            print("debug: delete events is noting.")
+#        
+#        b.set_event(
+#                date_=datetime.strptime(serializer.data["date"], "%Y-%m-%dT%H:%M:%S+09:00") - timedelta(hours=9),
+#                id_=serializer.data["uuid"])
+#        print("debug: perform_update")
     
     def perform_destroy(self, instance):
         b = CloudWatchEventsAccessor()
         try:
-            b.delete_event("fy19pu-reminder-" + (datetime.strptime(instance.data["date"], "%Y-%m-%dT%H:%M:%S+09:00") - timedelta(hours=9)).strftime("%Y%m%d%H%M"))
+            delete_event_name = "fy19pu-reminder-" + (datetime.strptime(instance.data["date"], "%Y-%m-%dT%H:%M:%S+09:00") - timedelta(hours=9)).strftime("%Y%m%d%H%M")
+            print("debug: delete event name:", delete_event_name)
+            b.delete_event(delete_event_name)
         except:
             print("debug: delete events is noting.")
         print("debug: perform_destroy")
